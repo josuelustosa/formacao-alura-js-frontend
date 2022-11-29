c0343d01a1e273a4e9bfe16fc77bd92e5775c2f0
Josué Lustosa <josuelustosa.job@gmail.com>
2022-11-29 10:44:06 -0400

    :construction: handling request return

--- 2-consumo-e-interacao-com-dados/2_2-consumindo-e-tratando-api/script.js
+++ 2-consumo-e-interacao-com-dados/2_2-consumindo-e-tratando-api/script.js
@@ -1,5 +1,13 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+var consultaCEP = fetch("https://viacep.com.br/ws/69058827/json/")
   .then((resposta) => resposta.json())
-  .then((r) => console.log(r));
+  .then((r) => {
+    if (r.erro) {
+      throw Error("Esse CEP não existe!");
+    } else {
+      console.log("Requisição bem-sucedida:\n", r);
+    }
+  })
+  .catch((erro) => console.log("Requisição falhou\n", erro))
+  .finally((mensagem) => console.log("Requisição Finalizada :)"));
 
 console.log(consultaCEP);

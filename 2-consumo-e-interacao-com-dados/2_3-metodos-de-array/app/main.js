7472384db5fa2ffb503619a45cacbc217b26bac6
Josué Lustosa <josuelustosa.job@gmail.com>
2022-12-27 15:33:42 -0400

    :construction: displaying books with foreach

--- 2-consumo-e-interacao-com-dados/2_3-metodos-de-array/app/main.js
+++ 2-consumo-e-interacao-com-dados/2_3-metodos-de-array/app/main.js
@@ -3,14 +3,38 @@ let livros = [];
 const endPointAPI =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 
+getBuscarLivrosDaAPI();
 
-getBuscarLivrosDaAPI()
+const elementoParaInserirNumeros = document.getElementById("livros");
 
 async function getBuscarLivrosDaAPI() {
-    const response = await fetch(endPointAPI)
-    livros = await response.json()
-    console.log("LIVROS DA ALURA: ", livros)
-    
-    // Console exibe o array em tabela
-    // console.table(livros)
+  const response = await fetch(endPointAPI);
+  livros = await response.json();
+
+  // Console exibe o array em tabela
+  console.table(livros);
+
+  exibirOsLivrosNaTela(livros);
+}
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+  listaDeLivros.forEach((livro) => {
+    elementoParaInserirNumeros.innerHTML += `
+    <div class="livro">
+    <img
+      class="livro__images"
+      src="${livro.imagem}"
+      alt="${livro.alt}"
+    />
+    <h2 class="livro__titulo">
+      ${livro.titulo}
+    </h2>
+    <p class="livro__descricao">${livro.autor}</p>
+    <p class="livro__preco" id="preco">${livro.preco}</p>
+    <div class="tags">
+      <span class="tag">${livro.categoria}</span>
+    </div>
+  </div>
+    `;
+  });
 }

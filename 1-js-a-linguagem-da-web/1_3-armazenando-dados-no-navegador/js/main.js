eb10a1568d0251ed679c1351cba05c9a92a771f2
Josué Lustosa <josuelustosa.job@gmail.com>
2023-03-21 10:42:46 -0400

    feat: creating item list

--- 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
+++ 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
@@ -1,9 +1,24 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
-  console.log(evento);
-  console.log(evento.target.elements['nome'].value);
-  console.log(evento.target.elements['quantidade'].value);
+  criarElemento(
+    (evento.target.elements["nome"].value ? evento.target.elements["nome"].value : 'Vazio'),
+    (evento.target.elements["quantidade"].value ? evento.target.elements["quantidade"].value : '0')
+  );
 });
+
+function criarElemento(nome, quantidade) {
+  const novoItem = document.createElement("li"); // cria uma tag li
+  novoItem.classList.add("item"); // add uma class na tag li criada acima
+
+  const numeroItem = document.createElement("strong"); // cria uma tag strong
+  numeroItem.innerHTML = quantidade;
+
+  novoItem.appendChild(numeroItem); // cria um elemento dentro de outro
+  novoItem.innerHTML += nome;
+
+  lista.appendChild(novoItem);
+}

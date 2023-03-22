4a2cb4607d1ffbac66ade026705a5eaaa332f453
Josué Lustosa <josuelustosa.job@gmail.com>
2023-03-22 17:02:06 -0400

    feat: Refresh page when registering item

--- 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
+++ 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
@@ -1,7 +1,11 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 
-const itens = []; // array de itens salvos
+const itens = JSON.parse(localStorage.getItem("itens")) || []; // array de itens salvos
+
+itens.forEach((elemento) => {
+  criarElemento(elemento);
+});
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
@@ -9,33 +13,30 @@ form.addEventListener("submit", (evento) => {
   const valueNome = evento.target.elements["nome"];
   const valueQuantidade = evento.target.elements["quantidade"];
 
-  criarElemento(
-    valueNome.value ? valueNome.value : "Vazio",
-    valueQuantidade.value ? valueQuantidade.value : "0"
-  );
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  criarElemento(itemAtual);
+
+  itens.push(itemAtual); // incluindo um item em um objeto de itens para não sobrepor o item anterior
+
+  localStorage.setItem("itens", JSON.stringify(itens)); // tranformando o array de itens em JSON com JSON.stringfy(), já que o localStorage só aceita string
 
   valueNome.value = "";
   valueQuantidade.value = "";
 });
 
-function criarElemento(nome, quantidade) {
+function criarElemento(item) {
   const novoItem = document.createElement("li"); // cria uma tag li
   novoItem.classList.add("item"); // add uma class na tag li criada acima
 
   const numeroItem = document.createElement("strong"); // cria uma tag strong
-  numeroItem.innerHTML = quantidade;
-
+  numeroItem.innerHTML = item.quantidade;
   novoItem.appendChild(numeroItem); // cria um elemento dentro de outro
-  novoItem.innerHTML += nome;
+
+  novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
-
-  const itemAtual = {
-    nome: nome,
-    quantidade: quantidade,
-  };
-
-  itens.push(itemAtual); // incluindo um item em um objeto de itens para não sobrepor o item anterior
-
-  localStorage.setItem("item", JSON.stringify(itens)); // tranformando o array de itens em JSON com JSON.stringfy(), já que o localStorage só aceita string
 }

5e5d81cd281d336df9e3beb2142ae8afe8bf59e9
Josué Lustosa <josuelustosa.job@gmail.com>
2023-03-24 15:12:54 -0400

    feat: Removing an Item from Backpack

--- 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
+++ 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
@@ -52,10 +52,27 @@ function criarElemento(item) {
 
   novoItem.innerHTML += item.nome;
 
+  novoItem.appendChild(botaoDeleta()); // cria o botão de deletar item da lista ('li')
+
   lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
   document.querySelector("[data-id='" + item.id + "']").innerHTML =
     item.quantidade;
+}
+
+function botaoDeleta() {
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerText = "X";
+
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode);
+  });
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag) {
+  tag.remove();
 }

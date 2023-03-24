6da78ad8d1a7b584e9eee4a612bd673f4f4ddabf
Josué Lustosa <josuelustosa.job@gmail.com>
2023-03-24 14:33:40 -0400

    refactor: Modify the quantity of an item

--- 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
+++ 1-js-a-linguagem-da-web/1_3-armazenando-dados-no-navegador/js/main.js
@@ -13,14 +13,25 @@ form.addEventListener("submit", (evento) => {
   const valueNome = evento.target.elements["nome"];
   const valueQuantidade = evento.target.elements["quantidade"];
 
+  // verificando se o item cadastrado já existe na lista
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
+
   const itemAtual = {
     nome: nome.value,
     quantidade: quantidade.value,
   };
 
-  criarElemento(itemAtual);
+  // verificando se o item cadastrado já existe na lista
+  if (existe) {
+    itemAtual.id = existe.id;
 
-  itens.push(itemAtual); // incluindo um item em um objeto de itens para não sobrepor o item anterior
+    atualizaElemento(itemAtual);
+  } else {
+    itemAtual.id = itens.length;
+    criarElemento(itemAtual);
+
+    itens.push(itemAtual); // incluindo um item em um objeto de itens para não sobrepor o item anterior
+  }
 
   localStorage.setItem("itens", JSON.stringify(itens)); // tranformando o array de itens em JSON com JSON.stringfy(), já que o localStorage só aceita string
 
@@ -34,9 +45,15 @@ function criarElemento(item) {
 
   const numeroItem = document.createElement("strong"); // cria uma tag strong
   numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
   novoItem.appendChild(numeroItem); // cria um elemento dentro de outro
 
   novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
 }
